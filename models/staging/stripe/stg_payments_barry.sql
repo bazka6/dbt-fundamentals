ba36534f545016af6fc433241f967a1d9c2863e9
barry.brown@south32.net <barry.brown@south32.net>
2022-04-29 06:08:29 +0000

    290422_3

--- models/staging/stripe/stg_payments_barry.sql
+++ models/staging/stripe/stg_payments_barry.sql
@@ -1,9 +1,10 @@
 select
-amount, 
-created,
-id,
-orderid,
-paymentmethod,
+amount/100 as amount,
+created as created_at,
+id as payment_id,
+orderid as order_id
+paymentmethod as payment_method
 status,
-_batched_at
+--unsure on below for batched_at
+_batched_at as payment_batched_at 
 from raw.stripe.payment
